6ba0ef5d67a0774e82952204c6b01cef942ed943
leogtzr <leogutierrezramirez@gmail.com>
2018-06-02 11:49:04 -0600

    Formatting code and added another test

--- readingpom_test.go
+++ readingpom_test.go
@@ -88,13 +88,83 @@ var (
 
 </project>
 `)
+
+	pomXml2 = []byte(`
+    <?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>org.batch</groupId>
+    <artifactId>validatingitems</artifactId>
+    <version>1.0-SNAPSHOT</version>
+
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>2.0.2.RELEASE</version>
+        <relativePath/> <!-- lookup parent from repository -->
+    </parent>
+
+    <properties>
+        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+        <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
+        <java.version>1.8</java.version>
+    </properties>
+
+    <dependencies>
+        
+        <!--
+        <dependency>
+            <groupId>com.h2database</groupId>
+            <artifactId>h2</artifactId>
+            <version>1.4.185</version>
+        </dependency>
+        -->
+        
+        <dependency>
+            <groupId>org.springframework</groupId>
+            <artifactId>spring-oxm</artifactId>
+            <version>4.1.2.RELEASE</version>
+        </dependency>
+
+    </dependencies>
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+    </build>
+
+</project>
+`)
 )
 
-func TestShouldFindExactNumberOfDependenciesInXMLFile(t *testing.T) {
+func TestShouldFindExactNumberOfDependenciesInXML(t *testing.T) {
 	deps := dependencies(pomXml)
 
-	const expected = 7
-	if len(deps) != expected {
-		t.Fatalf("Expecting: %d, got: %d", expected, len(deps))
+	const depsExpected = 7
+	if len(deps) != depsExpected {
+		t.Fatalf("Expecting: %d, got: %d", depsExpected, len(deps))
 	}
+}
+
+func TestShouldDeserializeStruct(t *testing.T) {
+
+	deps := dependencies(pomXml2)
+
+	numberOfDependenciesExpected := 1
+	if len(deps) != numberOfDependenciesExpected {
+		t.Fatalf("Expecting: %d, got: %d", numberOfDependenciesExpected, len(deps))
+	}
+
+	dep := deps[0]
+	if dep.GroupId == "" || dep.ArtifactId == "" || dep.Version == "" {
+		t.Fatal("Maven dependency wasn't found")
+	}
+
 }
