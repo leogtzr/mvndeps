ade12e243be0cdc349917c75ece363c6ba0db612
leogtzr <leogutierrezramirez@gmail.com>
2018-06-08 18:28:08 -0600

    fixes #1

--- mvndeps.go
+++ mvndeps.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"strings"
 )
 
 type Project struct {
@@ -13,13 +15,13 @@ type Project struct {
 }
 
 type Dependency struct {
-	GroupId    string `xml:"groupId"`
-	ArtifactId string `xml:"artifactId"`
+	GroupID    string `xml:"groupId"`
+	ArtifactID string `xml:"artifactId"`
 	Version    string `xml:"version"`
 }
 
 func (d Dependency) String() string {
-	return fmt.Sprintf("[groupId: %s], [artifactId: %s], [version: %s]", d.GroupId, d.ArtifactId, d.Version)
+	return fmt.Sprintf("[groupId: %s], [artifactId: %s], [version: %s]", d.GroupID, d.ArtifactID, d.Version)
 }
 
 func dependencies(content []byte) []Dependency {
@@ -28,26 +30,63 @@ func dependencies(content []byte) []Dependency {
 	return project.Dep
 }
 
-func main() {
+func showDependencies(deps []Dependency, fmtType *string) {
 
-	args := os.Args
-	if len(args) == 0 {
-		log.Fatal("Wrong number of arguments ... ")
+	switch strings.ToLower(*fmtType) {
+	case "xml":
+		for _, dep := range deps {
+			if output, err := xml.MarshalIndent(dep, "  ", "    "); err != nil {
+				fmt.Printf("error: %v\n", err)
+				continue
+			} else {
+				fmt.Println(string(output))
+			}
+		}
+	case "json":
+		for _, dep := range deps {
+			if data, err := json.MarshalIndent(dep, "", "    "); err != nil {
+				fmt.Printf("error: %v\n", err)
+				continue
+			} else {
+				fmt.Println(string(data))
+			}
+		}
+	case "text":
+		for _, dep := range deps {
+			fmt.Println(dep)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Wrong format\n")
+		os.Exit(1)
 	}
 
-	file := os.Args[1]
-	xmlFile, err := os.Open(file)
+}
+
+func main() {
+
+	fmtType := flag.String("type", "text", "Format type")
+	flag.Parse()
+
+	args := os.Args
+
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "Wrong number of arguments ... ")
+	}
+
+	file := flag.Args()
+	xmlFile, err := os.Open(file[0])
 
 	if err != nil {
-		log.Fatal("error opening file ... ", err)
+		fmt.Fprintln(os.Stderr, "error opening file ... ", err)
 	}
 
 	defer xmlFile.Close()
 
-	fileContent, _ := ioutil.ReadAll(xmlFile)
-	deps := dependencies(fileContent)
-
-	for _, dep := range deps {
-		fmt.Println(dep)
+	if fileContent, err := ioutil.ReadAll(xmlFile); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	} else {
+		deps := dependencies(fileContent)
+		showDependencies(deps, fmtType)
 	}
+
 }
